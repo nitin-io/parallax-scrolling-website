57ec2628c1d94c8946d190e824501a84e1cf2585
Nitin Chaudhary <nitin.io@outlook.com>
2022-10-19 21:06:48 +0530

    updated

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ let water = document.getElementById('water');
 let header = document.getElementById('header');
 
 window.addEventListener('scroll', function(){
-    let value = this.window.scrollY;
+    let value = window.scrollY;
 
-    text.style.top = value * -0.5 + '%';
+    text.style.top = 50 + value * -0.5 + '%';
     bird1.style.top = value * -1.5 + 'px';
     bird1.style.left = value * 2 + 'px';
-    bird2.style.left = value * -1.5 + 'px';
+    bird2.style.top = value * -1.5 + 'px';
     bird2.style.left = value * -5 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
     rocks.style.top = value * -0.12 + 'px';
